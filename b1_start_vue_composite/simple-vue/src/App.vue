04ae50c233bfd25fe81a31b49e2debd1e7c64a87
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-22 20:25:00 +0500

    b1_L4end Добавлены правильные стили в main.css

--- b1_start_vue_composite/simple-vue/src/App.vue
+++ b1_start_vue_composite/simple-vue/src/App.vue
@@ -15,7 +15,7 @@ const iceCreamFavors = ref([])
 
 
 <template>
-  <div class="shadow-deep">
+  <div>
     <h1>{{ header }}</h1>
     <input type="text" v-model.trim="newItem" placeholder="Add an item"/>
     Priority:
@@ -49,46 +49,5 @@ const iceCreamFavors = ref([])
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
 
-.shadow-deep {
-  box-shadow: 0 10px 15px rgba(0, 0, 0, 0.15),
-  0 4px 6px rgba(0, 0, 0, 0.1);
-  background: #fff;
-  border-radius: 10px;
-  padding: 20px;
-}
-
-li {
-  list-style: none;
-}
-
-ul{
-  padding: 10px;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
