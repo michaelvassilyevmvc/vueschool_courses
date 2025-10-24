8928a5947196ff8c33aec0d3951f575cd27fb60d
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-24 14:21:17 +0500

    b2_L9end Использование слотов

---
+++ b2_comp_fund_with_composite/next-app-fancy/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import FancyButton from '@/components/FancyButton.vue'
+</script>
+
+<template>
+  <div class="content">
+    <FancyButton>
+      <template #icon="{hover}">
+        {{ hover }}
+      </template>
+      <template #default> Submit </template>
+    </FancyButton>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ b2_comp_fund_with_composite/next-app-fancy/src/components/FancyButton.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @mouseenter="hover = true" @mouseleave="hover = false">
+    <slot name="icon" :hover="hover" >Default Value</slot>
+    <slot></slot>
+  </button>
+  {{hover}}
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const hover = ref(true)
+</script>
+
+<style scoped></style>
