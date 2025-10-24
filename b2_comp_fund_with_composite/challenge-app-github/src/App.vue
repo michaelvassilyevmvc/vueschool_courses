a74ef6bbc1de38f91c2fe8bbc8c4754d21f934c1
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-24 16:39:40 +0500

    b2_L11_self Стар задания реализации AppAlert

--- b2_comp_fund_with_composite/challenge-app-github/src/App.vue
+++ b2_comp_fund_with_composite/challenge-app-github/src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
-import GithubCard from '@/components/GithubCard.vue'
+// import GithubCard from '@/components/GithubCard.vue'
 </script>
 
 <template>
-  <GithubCard username="danielkellyio"></GithubCard>
-  <GithubCard username="hootlex"></GithubCard>
-  <GithubCard username="MooseSaeed"></GithubCard>
+
+
+
+<!--  GithubCard-->
+<!--  <GithubCard username="danielkellyio"></GithubCard>-->
+<!--  <GithubCard username="hootlex"></GithubCard>-->
+<!--  <GithubCard username="MooseSaeed"></GithubCard>-->
 </template>
 
 <style scoped></style>
