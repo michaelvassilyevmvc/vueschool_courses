be65e891d55960bb8f6fa81839d02080f3535960
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-24 20:57:34 +0500

    b2_L11_end Реализованы блоки Alert с помощью TailwindCss и DaizyUI. Реализовано удаление сообщений через массив

--- b2_comp_fund_with_composite/challenge-app-github/src/App.vue
+++ b2_comp_fund_with_composite/challenge-app-github/src/App.vue
@@ -1,9 +1,32 @@
 <script setup>
 // import GithubCard from '@/components/GithubCard.vue'
+import AppAlert from '@/components/AppAlert.vue'
+import { ref } from 'vue'
+
+const alerts = ref([
+  {id:1, type: 'info', message: 'New software update available.'},
+  {id:2, type: 'success', message: 'Your purchase has been confirmed!'},
+  {id:3, type: 'warning', message: 'Warning: Invalid email address!'},
+  {id:4, type: 'error', message: 'Error! Task failed successfully'},
+])
+// function handleClose() {
+//   console.log(this.alerts.value)
+//   console.log(id)
+//   // alerts.value = alerts.value.filter(alert => alert.value.id !== id)
+//   // console.log(alerts.value)
+// }
 </script>
 
 <template>
-
+  <div class="p-5">
+    <AppAlert v-for="alert in alerts"
+              @closed="alerts = alerts.filter(a => a.id !== alert.id)"
+              :key="alert.id"
+              :type="alert.type">
+      {{alert.message}}
+    </AppAlert>
+  </div>
+  <pre>{{ alerts }}</pre>
 
 
 <!--  GithubCard-->
--- b2_comp_fund_with_composite/challenge-app-github/src/components/AppAlert.vue
+++ b2_comp_fund_with_composite/challenge-app-github/src/components/AppAlert.vue
@@ -1,13 +1,50 @@
 <template>
-  <div>
-
+  <div role="alert" :class="`alert ${alertType}`" v-if="!closed">
+    <component :is="icon"></component>
+    <span><slot></slot></span>
+    <button @click="close">&times;</button>
   </div>
 </template>
 
 <script setup>
+import { computed, ref } from 'vue'
+import IconInfo from '@/components/icons/IconInfo.vue'
+import IconError from '@/components/icons/IconError.vue'
+import IconWarning from '@/components/icons/IconWarning.vue'
+import IconSuccess from '@/components/icons/IconSuccess.vue'
 
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'info',
+  },
+})
+
+const emit = defineEmits(['closed'])
+
+const closed = ref(false)
+function close() {
+  closed.value = true
+  emit('closed')
+}
+
+const alertType = computed(() => {
+  return {
+    info: 'alert-info',
+    warning: 'alert-warning',
+    error: 'alert-error',
+    success: 'alert-success',
+  }[props.type]
+})
+
+const icon = computed(() => {
+  return {
+    info: IconInfo,
+    warning: IconWarning,
+    error: IconError,
+    success: IconSuccess,
+  }[props.type]
+})
 </script>
 
-<style  scoped>
-
-</style>
+<style scoped></style>
