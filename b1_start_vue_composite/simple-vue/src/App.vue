a445ef07161216c517333a78e106c9291ae81769
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-24 09:25:11 +0500

    b1_L9_end Реализована обработка стилей на основе данных

--- b1_start_vue_composite/simple-vue/src/App.vue
+++ b1_start_vue_composite/simple-vue/src/App.vue
@@ -5,19 +5,29 @@ import {ref} from 'vue'
 const header = ref('Shopping List App')
 const editing = ref(false)
 const items = ref([
-//   {id: 1, label: "10 party hats"},
-//   {id: 2, label: "2 board games"},
-//   {id: 3, label: "20 cups}"}
+  {id: 1, label: "10 party hats", purchased: true, highPriority: false},
+  {id: 2, label: "2 board games", purchased: true, highPriority: false},
+  {id: 3, label: "20 cups", purchased: false, highPriority: true}
 ])
 const newItem = ref('')
-const newItemPriority = ref('low')
+// const newItemPriority = ref('low')
+const newItemHighPriority = ref(false)
 const saveItem = () => {
-  items.value.push({id: items.value.length + 1, label: newItem.value});
+  items.value.push({
+    id: items.value.length + 1,
+    label: newItem.value,
+    highPriority: newItemHighPriority.value});
   newItem.value = ''
+  newItemHighPriority.value = false
+
 }
 const doEdit = (e) => {
   editing.value = e
   newItem.value = ""
+  newItemHighPriority.value = false
+}
+const togglePurchased = (item) => {
+  item.purchased = !item.purchased;
 }
 </script>
 
@@ -38,20 +48,26 @@ const doEdit = (e) => {
           type="text"
           v-model.trim="newItem"
           placeholder="Add an item"/>
-      Priority:
-      <select v-model="newItemPriority">
-        <option value="low">low</option>
-        <option value="high">high</option>
-      </select>
+<!--      <select v-model="newItemPriority">-->
+<!--        <option value="low">low</option>-->
+<!--        <option value="high">high</option>-->
+<!--      </select>-->
+      <label style="padding-right:20px; display: inline-block;"> High Priority:
+      <input type="checkbox" v-model="newItemHighPriority" />
+      </label>
       <br>
       <button type="submit"
-              :disabled="newItem.length < 5"
               class="btn btn-primary">Save Item
       </button>
     </form>
     <ul>
-      <li v-for="{id, label} in items" :key="id">
-        {{ label }}
+      <li v-for="item in items"
+          @click="togglePurchased(item)"
+          :key="item.id"
+          class="static-class"
+          :class="{strikeout: item.purchased, priority: item.highPriority}"
+      >
+        {{ item.label }}
       </li>
     </ul>
     <p v-if="!items.length">
