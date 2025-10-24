6c952bbca90e2787a2e251a238f39d17946cd5a0
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-24 10:41:20 +0500

    b2_L1_end Создана кнопка счетчик

---
+++ b2_comp_fund_with_composite/vue-components-fundaments-with-the-composition-api/src/components/CounterButton.vue
@@ -0,0 +1,10 @@
+<template>
+  <button @click="($event) => count++">{{ count }}</button>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<style scoped></style>
