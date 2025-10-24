ce6f1ef5780640504132a6d12b6f33ca1f8b51f5
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-24 19:51:42 +0500

    b2_L11_self Реализованы блоки Alert с помощью TailwindCss и DaizyUI

--- b2_comp_fund_with_composite/challenge-app-github/src/App.vue
+++ b2_comp_fund_with_composite/challenge-app-github/src/App.vue
@@ -1,15 +1,24 @@
 <script setup>
 // import GithubCard from '@/components/GithubCard.vue'
+import AppAlert from '@/components/AppAlert.vue'
+
+function handleClose() {
+  console.log('closed')
+}
 </script>
 
 <template>
+  <div class="p-5">
+    <AppAlert @close="handleClose" type="info">New software update available.</AppAlert>
+    <AppAlert @close="handleClose" type="success">Your purchase has been confirmed!</AppAlert>
+    <AppAlert @close="handleClose" type="warning">Warning: Invalid email address!</AppAlert>
+    <AppAlert @close="handleClose" type="error" >Error! Task failed successfully</AppAlert>
+  </div>
 
-
-
-<!--  GithubCard-->
-<!--  <GithubCard username="danielkellyio"></GithubCard>-->
-<!--  <GithubCard username="hootlex"></GithubCard>-->
-<!--  <GithubCard username="MooseSaeed"></GithubCard>-->
+  <!--  GithubCard-->
+  <!--  <GithubCard username="danielkellyio"></GithubCard>-->
+  <!--  <GithubCard username="hootlex"></GithubCard>-->
+  <!--  <GithubCard username="MooseSaeed"></GithubCard>-->
 </template>
 
 <style scoped></style>
--- b2_comp_fund_with_composite/challenge-app-github/src/components/AppAlert.vue
+++ b2_comp_fund_with_composite/challenge-app-github/src/components/AppAlert.vue
@@ -1,13 +1,104 @@
 <template>
-  <div>
-
+  <div
+    role="alert"
+    class="alert m-2"
+    :class="[
+      {
+        'alert-info': props.type === 'info',
+        'alert-success': props.type === 'success',
+        'alert-warning': props.type === 'warning',
+        'alert-error': props.type === 'error',
+      },
+    ]"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      class="h-6 w-6 shrink-0 stroke-current"
+      v-if="props.type === 'info'"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      ></path>
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 shrink-0 stroke-current"
+      fill="none"
+      viewBox="0 0 24 24"
+      v-if="props.type === 'success'"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 shrink-0 stroke-current"
+      fill="none"
+      viewBox="0 0 24 24"
+      v-if="props.type === 'warning'"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+      />
+    </svg>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      class="h-6 w-6 shrink-0 stroke-current"
+      fill="none"
+      viewBox="0 0 24 24"
+      v-if="props.type === 'error'"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        stroke-width="2"
+        d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
+    </svg>
+    <span>
+      <slot> </slot>
+    </span>
+    <div>
+      <button @click="closeAlert" class="btn-close">&times;</button>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { computed } from 'vue'
 
+const props = defineProps({
+  type: {
+    type: String,
+    required: true,
+    validator: (value) => ['info', 'success', 'warning', 'error'].includes(value),
+  },
+})
+
+const emit = defineEmits(['close'])
+function closeAlert() {
+  emit('close')
+}
 </script>
 
-<style  scoped>
+<style scoped>
+.btn-close {
+  font-size: 25px;
+}
 
+.alert-red {
+  background-color: red;
+}
 </style>
