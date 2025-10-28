9d907e8589e5f31480dafef8296c37bc97b499cf
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-28 11:23:38 +0500

    b5_L1_end Добавление Transition

--- b5_transition/vue-transition/src/App.vue
+++ b5_transition/vue-transition/src/App.vue
@@ -1,47 +1,31 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import { ref } from 'vue'
+const showMenu = ref(false)
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  <button @click="showMenu = !showMenu">
+    {{ showMenu ? 'Hide' : 'Show' }}
+    Menu
+  </button>
+  <Transition>
+    <ul v-if="showMenu">
+      <li>Item 1</li>
+      <li>Item 2</li>
+      <li>Item 3</li>
+    </ul>
+  </Transition>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+/* we will explain what these classes do next! */
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
