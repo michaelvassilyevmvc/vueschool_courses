b01e3c892e3584346cf8bddaaffc71a1ac7416e0
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-29 15:07:18 +0500

    b6_L2_4_end Настройка динамического роутера для :id

---
+++ b6_masterclass/vuejs-masterclass/src/views/SingleProjectView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Project {{ route.params.id}}</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import {useRoute} from "vue-router";
+
+const route = useRoute();
+</script>
+
+<style scoped></style>
