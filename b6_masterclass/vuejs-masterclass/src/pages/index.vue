3fe823393809dad951fdf8105c195ff17b7b60a1
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-29 16:07:00 +0500

    b6_L2_7_end Файлы и директории настроены под unplugin-router

---
+++ b6_masterclass/vuejs-masterclass/src/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">
+      Go to project 1
+    </RouterLink>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
