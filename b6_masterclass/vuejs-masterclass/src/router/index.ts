b3ed8f569c6ed3d8d3114d9e98b9c3fe3ade80b9
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-29 15:40:19 +0500

    b6_L2_6_end Настройка unplugin-vue-router для файловой маршрутизации

--- b6_masterclass/vuejs-masterclass/src/router/index.ts
+++ b6_masterclass/vuejs-masterclass/src/router/index.ts
@@ -1,31 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import { h } from 'vue'
+import { routes } from 'vue-router/auto-routes'
+
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/projects',
-      name: 'projects',
-      component: () => import('@/views/ProjectsView.vue')
-    },
-    {
-      path: '/projects/:id',
-      name: 'single-project',
-      component: () => import('@/views/SingleProjectView.vue')
-    },
-    {
-      path: '/:catchAll(.*)*',
-      name: 'NotFound',
-      component: h('p', { style: 'color:red;' }, '404 Project Not Found')
-    }
-  ]
+  routes
 })
 
 export default router
