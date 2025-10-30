bd7eeb3267e4bb0c806141b782644f769b33076e
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-30 20:23:55 +0500

    b6_L4_1_end Добавлены tailwind, autoprefixer, shadcn для работы с будущими компонентами

--- b6_masterclass/vuejs-masterclass/vite.config.ts
+++ b6_masterclass/vuejs-masterclass/vite.config.ts
@@ -3,13 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueRouter from 'unplugin-vue-router/vite'
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    VueRouter({}),
-    vue(),
-  ],
+  plugins: [VueRouter({}), vue()],
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
+    }
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
