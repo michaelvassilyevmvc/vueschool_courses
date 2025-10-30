71d8d6ce28794a47ce1378fe3f44072329350ad7
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-30 14:07:21 +0500

    b6_L3_9_end Создана связь между удаленной БД и файлом seed.js

--- b6_masterclass/vuejs-masterclass/database/seed.js
+++ b6_masterclass/vuejs-masterclass/database/seed.js
@@ -1,5 +1,12 @@
+/* eslint-env node */
+
 import {fakerEN_US as faker} from '@faker-js/faker'
 
-const personName = faker.person.fullName()
-const personBio = faker.person.bio()
-console.log(`Hi! My name is ${personName}! I am a ${personBio}!`)
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.VITE_SUPABASE_URL,
+  process.env.SERVICE_ROLE_KEY
+)
+
+console.log(supabase)
