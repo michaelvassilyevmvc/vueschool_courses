60dd2cd8bebff1a13905ab04ea0870da219ae73b
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-30 20:35:16 +0500

    b6_L4_2_end Добавлена разметка для стартовой страницы с классами tailwindcss

--- b6_masterclass/vuejs-masterclass/src/App.vue
+++ b6_masterclass/vuejs-masterclass/src/App.vue
@@ -1,10 +1,14 @@
 <script setup lang="ts">
-import { supabase } from '@/lib/supabaseClient'
 
-console.log(supabase)
 </script>
 
 <template>
+  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
+    <form class="w-full max-w-96">
+      <input class="w-full bg-background pl-8" type="text" placeholder="Search...">
+    </form>
+    <div class="w-8 h-8 rounded-full bg-white"></div>
+  </nav>
   <main>
     <RouterView />
   </main>
