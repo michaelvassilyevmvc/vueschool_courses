0f55e71c94afe3d9fd2577455aaaa2a605b75f23
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-31 16:29:43 +0500

    b6_L5_2_end Обновление vue-router для использования autoimport-vue-router

--- b6_masterclass/vuejs-masterclass/vite.config.ts
+++ b6_masterclass/vuejs-masterclass/vite.config.ts
@@ -6,6 +6,7 @@ import VueRouter from 'unplugin-vue-router/vite'
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -29,7 +30,7 @@ export default defineConfig({
       imports: [
         // presets
         'vue',
-        'vue-router'
+        VueRouterAutoImports,
       ],
       dts: true,
       viteOptimizeDeps: true,
