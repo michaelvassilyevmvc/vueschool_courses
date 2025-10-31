dc15fc4c2aaf0b44fb0018632a7c5551403bdc20
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-31 09:57:41 +0500

    b6_L4_10_end Добавлен компонент для полнофункциональной таблицы

--- b6_masterclass/vuejs-masterclass/src/pages/tasks/index.vue
+++ b6_masterclass/vuejs-masterclass/src/pages/tasks/index.vue
@@ -1,21 +1,16 @@
 <template>
-  <div>
-    <h1>Tasks Page</h1>
-    <RouterLink :to="{name:'/'}">Go to home</RouterLink>
-    <ul>
-      <li v-for="task in tasks" :key="task.id">
-        {{task.name}}
-      </li>
-    </ul>
-  </div>
+  <DataTable :columns="columns" :data="payments" />
 </template>
 
 <script setup lang="ts">
 import { supabase } from '@/lib/supabaseClient'
 import { ref } from 'vue'
 import type { Tables } from '../../../database/types'
+import { h } from 'vue'
+import type { ColumnDef } from '@tanstack/vue-table'
+import DataTable from '@/components/ui/data-table/DataTable.vue'
 
-const tasks = ref<Tables<'tasks'>[] | null>(null);
+const tasks = ref<Tables<'tasks'>[] | null>(null)
 
 ;(async () => {
   const { data, error } = await supabase.from('tasks').select()
@@ -24,6 +19,47 @@ const tasks = ref<Tables<'tasks'>[] | null>(null);
   }
   tasks.value = data
 })()
+
+interface Payment {
+  id: string
+  amount: number
+  status: 'pending' | 'processing' | 'success' | 'failed'
+  email: string
+}
+
+const payments: Payment[] = [
+  {
+    id: '728ed52f',
+    amount: 100,
+    status: 'pending',
+    email: 'm@example.com'
+  },
+  {
+    id: '489e1d42',
+    amount: 125,
+    status: 'processing',
+    email: 'example@gmail.com'
+  }
+  // ...
+]
+
+
+
+const columns: ColumnDef<Payment>[] = [
+  {
+    accessorKey: 'amount',
+    header: () => h('div', { class: 'text-right' }, 'Amount'),
+    cell: ({ row }) => {
+      const amount = Number.parseFloat(row.getValue('amount'))
+      const formatted = new Intl.NumberFormat('en-US', {
+        style: 'currency',
+        currency: 'USD',
+      }).format(amount)
+
+      return h('div', { class: 'text-right font-medium' }, formatted)
+    },
+  }
+]
 </script>
 
 <style scoped></style>
