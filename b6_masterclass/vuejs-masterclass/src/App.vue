271f807c5049d4b681094ec3af09e0755054cd8b
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-10-31 05:35:10 +0500

    b6_L4_6_end Реализация TopNavbar и Sidebar компонентов

--- b6_masterclass/vuejs-masterclass/src/App.vue
+++ b6_masterclass/vuejs-masterclass/src/App.vue
@@ -1,42 +1,15 @@
 <script setup lang="ts">
-import { Input } from '@/components/ui/input'
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '@/components/ui/dropdown-menu'
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+
+import TopNavbar from '@/components/Layout/TopNavbar.vue'
+import Sidebar from '@/components/Layout/Sidebar.vue'
 </script>
 
 <template>
-  <nav class="h-16 border-b bg-muted/40 flex justify-between items-center px-6">
-    <form class="w-full max-w-96 relative h-fit">
-      <iconify-icon class="absolute top-[50%] translate-y-[-50%] left-2.5 text-muted-foreground" icon="lucide:search"></iconify-icon>
-      <Input class="w-full bg-background pl-8" type="text" placeholder="Search..." />
-    </form>
-    <DropdownMenu>
-      <DropdownMenuTrigger>
-        <Avatar>
-          <!--          <AvatarImage src="https://github.com/unovue.png" alt="@unovue" />-->
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent>
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem>Billing</DropdownMenuItem>
-        <DropdownMenuItem>Team</DropdownMenuItem>
-        <DropdownMenuItem>Subscription</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  </nav>
-  <main>
-    <RouterView />
-  </main>
+  <Sidebar />
+  <div class="flex flex-col lg:ml-52 ml-16 transition-[margin]">
+    <TopNavbar />
+    <main>
+      <RouterView />
+    </main>
+  </div>
 </template>
-
-<style scoped></style>
