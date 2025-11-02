4f99b7d6dccc25c497b621bcc0d0a94a73e020d5
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-02 18:01:43 +0500

    b6_L7_21_end Настроена обработка ошибок для каждого поля ввода страницы login

--- b6_masterclass/vuejs-masterclass/src/composables/formErrors.ts
+++ b6_masterclass/vuejs-masterclass/src/composables/formErrors.ts
@@ -1,12 +1,29 @@
 import type { AuthError } from '@supabase/supabase-js'
+import type { LoginForm } from '@/types/AuthForm'
 
 export const useFormErrors = () => {
   const serverError = ref('')
+  const realtimeError = ref()
 
   const handleServerError = (error: AuthError) => {
     serverError.value =
       error.message === 'Invalid login credentials' ? 'Incorrect email or password' : error.message
   }
 
-  return { serverError, handleServerError }
+  const handleLoginError = async (formData: LoginForm) => {
+    realtimeError.value = {
+      email: [],
+      password: []
+    }
+
+    const { validateEmail, validatePassword } = await import('@/utils/formValidations')
+
+    const emailErrors = validateEmail(formData.email)
+    if (emailErrors.length) realtimeError.value.email = emailErrors
+
+    const passwordErrors = validatePassword(formData.password)
+    if (passwordErrors.length) realtimeError.value.password = passwordErrors
+  }
+
+  return { serverError, handleServerError, realtimeError, handleLoginError }
 }
--- b6_masterclass/vuejs-masterclass/src/pages/login.vue
+++ b6_masterclass/vuejs-masterclass/src/pages/login.vue
@@ -2,19 +2,18 @@
 import { login } from '@/utils/supaAuth'
 import { useFormErrors } from '@/composables/formErrors'
 
-
 const formData = ref({
   email: '',
   password: ''
 })
 
-const {serverError, handleServerError} = useFormErrors()
+const { serverError, handleServerError, realtimeError, handleLoginError } = useFormErrors()
 
 const router = useRouter()
 
 const signin = async () => {
   const { error } = await login(formData.value)
-  if(!error) return router.push('/')
+  if (!error) return router.push('/')
 
   handleServerError(error)
 }
@@ -42,7 +41,13 @@ const signin = async () => {
               required
               v-model="formData.email"
               :class="{ 'border-red-500': serverError }"
+              @input="handleLoginError(formData)"
             />
+            <ul class="text-sm text-left text-red-500" v-if="realtimeError?.email.length">
+              <li v-for="error in realtimeError?.email" :key="error" class="list-disc">
+                {{ error }}
+              </li>
+            </ul>
           </div>
           <div class="grid gap-2">
             <div class="flex items-center">
@@ -56,7 +61,13 @@ const signin = async () => {
               required
               v-model="formData.password"
               :class="{ 'border-red-500': serverError }"
+              @input="handleLoginError(formData)"
             />
+            <ul class="text-sm text-left text-red-500" v-if="realtimeError?.password.length">
+              <li v-for="error in realtimeError?.password" :key="error" class="list-disc">
+                {{ error }}
+              </li>
+            </ul>
           </div>
           <ul class="text-sm text-left text-red-500" v-if="serverError">
             <li class="list-disc">{{ serverError }}</li>
