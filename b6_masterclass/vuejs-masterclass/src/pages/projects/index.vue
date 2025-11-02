b7efe442b7f938710f9730f7243eb4a662be2a6c
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-02 20:40:25 +0500

    b6_L8_2_end Создание кеширивания для страницы projects

--- b6_masterclass/vuejs-masterclass/src/pages/projects/index.vue
+++ b6_masterclass/vuejs-masterclass/src/pages/projects/index.vue
@@ -3,21 +3,14 @@
 </template>
 
 <script setup lang="ts">
-import { projectsQuery } from '@/utils/supaQueries'
-import type { Projects } from '@/utils/supaQueries'
 import { columns } from '@/utils/tableColumns/projectsColumns'
+import { useProjectsStore } from '@/stores/loaders/projects'
 
 usePageStore().pageData.title = 'Projects'
 
-const projects = ref<Projects>([])
-
-const getProjects = async () => {
-  const { data, error, status } = await projectsQuery()
-  if (error) {
-    useErrorStore().setError({ error, customCode: status })
-  }
-    projects.value = data ?? []
-}
+const projectsLoader = useProjectsStore()
+const { projects } = storeToRefs(projectsLoader)
+const { getProjects } = projectsLoader
 
 await getProjects()
 </script>
