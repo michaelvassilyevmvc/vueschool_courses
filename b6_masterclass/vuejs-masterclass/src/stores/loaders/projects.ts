d9fa0b979911dfa09fadc42161989efd9920e0d9
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-02 20:56:44 +0500

    b6_L8_4_end Обновление кеша при разности с данными

--- b6_masterclass/vuejs-masterclass/src/stores/loaders/projects.ts
+++ b6_masterclass/vuejs-masterclass/src/stores/loaders/projects.ts
@@ -1,10 +1,23 @@
 import { projectsQuery } from '@/utils/supaQueries'
 import type { Projects } from '@/utils/supaQueries'
-import {useMemoize} from '@vueuse/core'
+import { useMemoize } from '@vueuse/core'
 
 export const useProjectsStore = defineStore('projects-store', () => {
   const projects = ref<Projects>([])
   const loadProjects = useMemoize(async (key: string) => await projectsQuery())
+  const validateCache = () => {
+    if (projects.value.length) {
+      projectsQuery().then(({ data }) => {
+        if (JSON.stringify(data) === JSON.stringify(projects.value)) {
+          console.log('Cached and fresh data matched!')
+          return
+        } else {
+          console.log('Something has chaanged')
+          loadProjects.delete('projects')
+        }
+      })
+    }
+  }
 
   const getProjects = async () => {
     const { data, error, status } = await loadProjects('projects')
@@ -12,6 +25,8 @@ export const useProjectsStore = defineStore('projects-store', () => {
       useErrorStore().setError({ error, customCode: status })
     }
     projects.value = data ?? []
+
+    validateCache()
   }
 
   return {
