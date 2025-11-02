89982c111bd11904f8609f66d3cfb619bfe679eb
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-02 16:18:09 +0500

    b6_L7_16_end Создано перенаправление на страницу login в случае logout

--- b6_masterclass/vuejs-masterclass/src/components/Layout/Sidebar.vue
+++ b6_masterclass/vuejs-masterclass/src/components/Layout/Sidebar.vue
@@ -25,10 +25,16 @@
 </template>
 
 <script setup lang="ts">
+const router = useRouter();
+
 const executeAction = async (linkTitle: string) => {
   if (linkTitle === 'Sign Out') {
     const { logout } = await import('@/utils/supaAuth')
-    await logout()
+    const isLoggedOut = await logout()
+
+    if (isLoggedOut) {
+      router.push('/login')
+    }
   }
 }
 
--- b6_masterclass/vuejs-masterclass/src/router/index.ts
+++ b6_masterclass/vuejs-masterclass/src/router/index.ts
@@ -6,10 +6,14 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes
 })
-//
-// router.beforeEach(async () => {
-//   const { getSession } = useAuthStore()
-//   await getSession()
-// })
+
+router.beforeEach(async (to, from) => {
+  const { user } = storeToRefs(useAuthStore())
+  if (!user.value && !['/login','/register'].includes(to.path)) {
+    return {
+      name: '/login'
+    }
+  }
+})
 
 export default router
