f8ac2d49fb999c5804e201512bafa7ca2c16f947
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-08 12:25:29 +0500

    b6_L8_25_end  Создание формы для нового task

--- b6_masterclass/vuejs-masterclass/src/components/AppNew/AppNewTask.vue
+++ b6_masterclass/vuejs-masterclass/src/components/AppNew/AppNewTask.vue
@@ -4,22 +4,49 @@
       <SheetHeader>
         <SheetTitle>Create new task</SheetTitle>
       </SheetHeader>
-      <FormKit type="text" />
+      <FormKit type="form" @submit="createTask" submit-label="Create task">
+        <FormKit type="text" name="name" id="name" label="Name" placeholder="My new task" />
+        <FormKit
+          type="select"
+          name="for"
+          id="for"
+          label="For"
+          placeholder="Select a user"
+          :options="[{ label: 'Item name', value: 1 }]"
+        />
+        <FormKit
+          type="textarea"
+          name="description"
+          id="description"
+          label="Description"
+          placeholder="Task description"
+        />
+        <FormKit
+          type="select"
+          name="project"
+          id="project"
+          label="Project"
+          placeholder="Select a project"
+          :options="[{ label: 'Item name', value: 1 }]"
+        />
+      </FormKit>
     </SheetContent>
   </Sheet>
 </template>
 
 <script setup lang="ts">
-import {
-  Sheet,
-  SheetContent,
-  SheetDescription,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger
-} from '@/components/ui/sheet'
+import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
+import type { CreateNewTask } from '@/types/CreateNewForm'
 
 const sheetOpen = defineModel<boolean>()
+
+const createTask = async (formData: CreateNewTask) => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(console.log(formData))
+    }, 2000)
+  })
+}
 </script>
 
 <style scoped></style>
