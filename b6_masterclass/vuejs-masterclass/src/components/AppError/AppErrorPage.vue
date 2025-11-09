9adfa35c08e172b42eb2e8787ecfd26a1c4dd43e
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-09 12:34:03 +0500

    b6_L8_end  After merge

--- b6_masterclass/vuejs-masterclass/src/components/AppError/AppErrorPage.vue
+++ b6_masterclass/vuejs-masterclass/src/components/AppError/AppErrorPage.vue
@@ -8,22 +8,22 @@
 <style scoped>
 .error {
   @apply mx-auto flex justify-center items-center flex-1 p-10 text-center -mt-20 min-h-[90vh];
-}
+
 
   @apply text-7xl text-destructive;
-}
+
 
   @apply font-extrabold text-7xl text-secondary;
-}
+
 
   @apply text-3xl font-extrabold text-primary;
-}
+
 
   @apply flex flex-col items-center justify-center gap-5 mt-6 font-light;
-}
+
 
   @apply text-lg text-muted-foreground;
-}
+
 
   @apply my-2;
 }
