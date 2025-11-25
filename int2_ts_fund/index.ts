2ddfc2d243d790acb7734b84fd2aaf83311290ac
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-25 14:32:43 +0500

    int2_ch4_L3_end Использование generic

--- int2_ts_fund/index.ts
+++ int2_ts_fund/index.ts
@@ -1,77 +1,20 @@
-enum Sizes {
-  small,
-  medium = 'medium',
-  large = 'large'
+interface Person {
+  name: string;
+  age: number;
 }
 
-interface Emailable {
-  emailBody(): string,
-
-  emailSubject(): string
+const me: Person = {
+  name: 'Juan',
+  age: 25
 }
 
-class InventoryItem {
-  name: string
-  price: number
-
-  constructor(name: string, price: number) {
-    this.name = name;
-    this.price = price;
-  }
-
-
-  buy(): this {
-    console.log(this.price);
-    return this
-  }
-
+const clone = <T>(value: T): T => {
+  const json = JSON.stringify(value);
+  return JSON.parse(json)
 }
 
-class Product extends InventoryItem {
-  color: string = 'gray'
-   size: Sizes | undefined
+const myClone = clone(me);
 
-}
-
-
-class Service extends InventoryItem implements Emailable {
-  startTime: Date
-  endTime: Date
-
-
-  constructor(name: string, price: number, startTime: Date, endTime: Date) {
-    super(name, price);
-    this.startTime = startTime;
-    this.endTime = endTime;
-  }
-
-  emailBody(): string {
-    return `Thank you for purchasing ${this.name}!Your appointment starts at ${this.startTime.toLocaleTimeString()} and ends at ${this.endTime.toLocaleTimeString()}.`
-  }
-
-  emailSubject(): string {
-    return `${this.name} | My Company`
-  }
-}
-
-function sendEmail(emailable: Emailable, to: string) {
-  console.log('Body:', emailable.emailBody());
-  console.log('Subject::', emailable.emailSubject());
-  console.log('To:', to);
-}
-
-const tshirt = new Product('TShirt Design B', 10);
-tshirt.size = Sizes.medium;
-tshirt.color = 'red';
-tshirt.buy();
-
-const photoShoot = new Service(
-    'Kelly Wedding Photo Shoot',
-    450,
-    new Date('May 9, 2009 11:00:00'),
-    new Date('May 9, 2009 12:30:00'),
-)
-
-photoShoot.buy()
-
-sendEmail(photoShoot, 'rebusmv@gmail.com')
+const books: string[] = ['Harry Potter', 'Jurassic Park', 'The Dark Tower'];
+const bookCopies = clone<string[]>(books);
+bookCopies.push('Project Hail Mary');
