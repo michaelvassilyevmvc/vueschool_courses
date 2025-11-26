9b22e9b8920ad7b4317a7ec9dfca54b0d02c3848
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-26 15:10:33 +0500

    int3_ch2_L5_end Обработка событий ввода текста и установка ограничения

--- int3_ts_vue/ts-journal/src/components/EntryEditor.vue
+++ int3_ts_vue/ts-journal/src/components/EntryEditor.vue
@@ -7,16 +7,26 @@ import type Emoji from "@/types/Emoji";
 const text = ref("");
 const emoji = ref<Emoji | null>(null);
 const charCount = computed<number>(() => text.value.length);
+const maxCount = 280;
+const handleTextInput = (e: Event) => {
+  const textarea = e.target as HTMLTextAreaElement;
+  if (textarea.value.length <= maxCount) {
+    text.value = textarea.value;
+  } else {
+    text.value = textarea.value = textarea.value.substring(0, maxCount);
+  }
+};
 </script>
 <template>
   <form class="entry-form" @submit.prevent>
     <textarea
-      v-model="text"
+      :value="text"
+      @keyup="handleTextInput"
       placeholder="New Journal Entry for danielkelly_io"
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>{{ charCount }} / 280</span>
+      <span>{{ charCount }} / {{ maxCount}}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
