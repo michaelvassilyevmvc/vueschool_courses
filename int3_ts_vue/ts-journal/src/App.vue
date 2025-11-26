6bb0f7d10f519d39d34eb5ebf432bb287f0c38e8
michaelvassilyevmvc <michaelvassilyevmvc@gmail.com>
2025-11-26 17:29:36 +0500

    int3_ch2_L8_end Передача объекта для сохранения

--- int3_ts_vue/ts-journal/src/App.vue
+++ int3_ts_vue/ts-journal/src/App.vue
@@ -5,6 +5,7 @@ import EntryCard from "@/components/EntryCard.vue";
 
 import { reactive } from "vue";
 import type User from "@/types/User";
+import type Entry from "@/types/Entry";
 
 const user: User = reactive({
   id: 1,
@@ -12,12 +13,16 @@ const user: User = reactive({
   settings: [],
 });
 console.log(user.id);
+
+const handleCreateEntry = (entry: Entry) => {
+  console.log(entry);
+}
 </script>
 
 <template>
   <main class="container m-auto p-10">
     <TheHeader />
-    <EntryEditor  />
+    <EntryEditor @@create="handleCreateEntry"  />
     <ul>
       <li>
         <EntryCard />
--- int3_ts_vue/ts-journal/src/components/EntryEditor.vue
+++ int3_ts_vue/ts-journal/src/components/EntryEditor.vue
@@ -3,36 +3,50 @@ import EmojiField from "@/components/EmojiField.vue";
 import ArrowCircleRight from "@/assets/icons/arrow-circle-right.svg";
 import { computed, ref } from "vue";
 import type Emoji from "@/types/Emoji";
+import type Entry from "@/types/Entry";
 
 // data
-const text = ref("");
+const body = ref("");
 const emoji = ref<Emoji | null>(null);
-const charCount = computed<number>(() => text.value.length);
+const charCount = computed<number>(() => body.value.length);
 const maxCount = 280;
 
 // methods
 const handleTextInput = (e: Event) => {
   const textarea = e.target as HTMLTextAreaElement;
   if (textarea.value.length <= maxCount) {
-    text.value = textarea.value;
+    body.value = textarea.value;
   } else {
-    text.value = textarea.value = textarea.value.substring(0, maxCount);
+    body.value = textarea.value = textarea.value.substring(0, maxCount);
   }
 };
 
 // events
-defineEmits(["@create"]);
+defineEmits<{
+  (e: "@create", entry: Entry): void;
+}>();
 </script>
 <template>
-  <form class="entry-form" @submit.prevent="$emit('@create',{text, emoji})">
+  <form
+    class="entry-form"
+    @submit.prevent="
+      $emit('@create', {
+        createdAt: new Date(),
+        id: Math.random(),
+        userId: 1,
+        body,
+        emoji,
+      })
+    "
+  >
     <textarea
-      :value="text"
-      @keyup="handleTextInput"
+      :value="body"
       placeholder="New Journal Entry for danielkelly_io"
+      @keyup="handleTextInput"
     ></textarea>
     <EmojiField v-model="emoji" />
     <div class="entry-form-footer">
-      <span>{{ charCount }} / {{ maxCount}}</span>
+      <span>{{ charCount }} / {{ maxCount }}</span>
       <button>Remember <ArrowCircleRight width="20" /></button>
     </div>
   </form>
